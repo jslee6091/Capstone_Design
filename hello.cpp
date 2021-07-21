beaaa049f2c6a0d89727e6c961bd969c357b5c3c
jslee6091 <jslee6091@naver.com>
2021-07-21 23:58:00 +0900

    input_output

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, char* argv[]){
     std::cout << "hello world" << std::endl;
+    int number;
+    // cin과 cout은 std를 선언해주거나 std::cout 이렇게 사용해야 한다.
+    cin >> number;
+    cout << number << "\n";
     printf("hello!!\n");
     return 0;
 }
